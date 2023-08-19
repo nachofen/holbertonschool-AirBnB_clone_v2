0b93d7f9ec9157db1d5b34625b072695ae44ac5e
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-19 12:44:11 -0700

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,26 @@
-<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-        <UL>
-        {% for state in states.values() | sort(attribute='name') %}
-            <LI>{{ state.id }}: <B>{{ state.name }}</B>
-                <UL>
-                {% for city in state.cities | sort(attribute='name')%}
-                    <LI>{{ city.id }}: <B>{{ city.name }}</B></LI>
-                {% endfor %}
-                </UL>
-            </LI>
-        {% endfor %}
-        </UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""
+This module defines a simple Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def app_teardown_appcontext(self):
+    """After each request this removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
